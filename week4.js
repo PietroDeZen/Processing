f98d013ad60bf2123f9f4c5f00ac118d36ce631e
PietroDeZen <111724497+PietroDeZen@users.noreply.github.com>
2022-09-05 16:05:09 +1000

    Initialize repository

---
+++ week4.js
@@ -0,0 +1,18 @@
+/*
+    Paste the code for your week 4 exercise below.
+*/
+var w = window.innerWidth;
+var h = window.innerHeight;
+function setup() {
+    createCanvas(500, 500);
+  }
+  
+ 
+  
+  let midx = 250;
+  let midy = 250;
+  
+  function draw() {
+    circle(midx, midy, 100);
+  }
+  
